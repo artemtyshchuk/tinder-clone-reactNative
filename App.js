e0b8c999706527fbb341e57be279aa0f521e93d5
Artem Tyshchuk <artemtyschukk@gmail.com>
2024-04-21 23:35:31 +0200

    added an auth component

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { AuthProvider } from "./hooks/useAuth";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const user = true;
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl font-bold">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <AuthProvider>
+          <Stack.Navigator>
+            {user ? (
+              <>
+                <Stack.Screen
+                  name="HomeScreen"
+                  component={HomeScreen}
+                  // options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="ChatScreen"
+                  component={ChatScreen}
+                  // options={{ headerShown: false }}
+                />
+              </>
+            ) : (
+              <Stack.Screen
+                name="LoginScreen"
+                component={LoginScreen}
+                // options={{ headerShown: false }}
+              />
+            )}
+          </Stack.Navigator>
+        </AuthProvider>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
